689f7d7032a0288c7011d09087628c3caba50987
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-26 15:13:23 +0300

    Proje ve Database Kolonları Oluşturuldu

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("mysql_connection");
+    //options.UseSqlite(connectionString);
+    var version = new MySqlServerVersion(new Version(8, 2, 0));
+    options.UseMySql(connectionString, version);
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();

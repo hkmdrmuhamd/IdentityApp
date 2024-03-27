b1988d6b3ed080871b411494cd8ae3ae0d6174d5
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-27 13:21:25 +0300

    Identity Options

--- ViewModels/CreateViewModel.cs
+++ ViewModels/CreateViewModel.cs
@@ -18,4 +18,11 @@ namespace IdentityApp.ViewModels
         [Compare(nameof(Password), ErrorMessage = "Parolalar eşleşmiyor.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
+    // Identity projesi oluşturduğumuzda parola olarak belirlediğimiz alan için validasyon ayarları şu şekilde default olarak gelir. Bu alanları ekstra belirtmemize gerek yoktur
+    // options.Password.RequireDigit = true;
+    // options.Password.RequireLowercase = true;
+    // options.Password.RequireNonAlphanumeric = true;
+    // options.Password.RequireUppercase = true;
+    // options.Password.RequiredLength = 6;
+    // options.Password.RequiredUniqueChars = 1;
 }
